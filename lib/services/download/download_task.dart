8f398939cf3e46b833aecf61bd919424fc89e3dd
snylonue <snylonue@gmail.com>
2023-06-25 21:25:34 +0800

    dont't follow redirect

--- lib/services/download/download_task.dart
+++ lib/services/download/download_task.dart
@@ -57,7 +57,7 @@ class DownloadTask extends ChangeNotifier {
       final response = await client.download(
         url,
         '$savePath.tmp',
-        options: Options(headers: headers),
+        options: Options(headers: headers, followRedirects: false),
         onReceiveProgress: (final count, final total) {
           status = DownloadTaskStatus.downloading;
           progress =
